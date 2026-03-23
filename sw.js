20ae34aed272d35ab72296de021d09a469184ee1
Gabriel Bedell <gabriel_bedell@Gabriels-MacBook-Air.local>
2026-03-22 22:26:57 -0400

    initial commit

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'skincare-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
